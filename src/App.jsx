935f25d9326a4ca090bd064fbc8f0b78bf924d8d
Hemanth R <hemanthrt18@gmail.com>
2023-03-27 10:11:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Navbar } from './components'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Navbar />
+      <h1>Project Hippo 🦛</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='nav__logo'>
+            🦛
+        </div>
+        <h2>H.I.P.P.O</h2>
+    </div>
+  )
+}
+
+export default Navbar
