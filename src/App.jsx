4fc090d0ec2e0deefb4093f55be099f2bf1590d6
Hemanth R <hemanthrt18@gmail.com>
2023-04-05 09:22:08 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import React, { useState, useEffect } from 'react'
 
 import { Navbar } from './components'
 import './App.css'
 
 function App() {
+  const [currentTabUrl, setCurrentTabUrl] = useState("");
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length > 0) {
+        setCurrentTabUrl(tabs[0]?.url);
+      }
+    });
+  }, []);
+
+  console.log(currentTabUrl)
 
   return (
     <div className="app">
       <Navbar />
       <h1>Project Hippo 🦛</h1>
+      <h5>{currentTabUrl}</h5>
     </div>
   )
 }
