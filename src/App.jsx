e9d800d988e897464469013a6bf78310cde590d1
Hemanth R <hemanthrt18@gmail.com>
2023-04-06 10:29:33 +0530

    🦛

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,31 @@ import './App.css'
 function App() {
   const [currentTabUrl, setCurrentTabUrl] = useState("");
 
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        setCurrentTabUrl(tabs[0]?.url);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     if (tabs.length > 0) {
+  //       setCurrentTabUrl(tabs[0]?.url);
+  //     }
+  //   });
+  // }, []);
+
+  const chrome = window.chrome;
+
+
+setTimeout(function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    const url = tabs[0].url;
+    setCurrentTabUrl(url);
+  });
+}, 1000); // Wait for 1 second before using the chrome API
+
 
   console.log(currentTabUrl)
 
   return (
     <div className="app">
       <Navbar />
-      <h1>Project Hippo 🦛</h1>
+      <h1>Project Hippoooooo 🦛</h1>
       <h5>{currentTabUrl}</h5>
     </div>
   )
