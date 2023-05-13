77e4cd136d2c57b71a50599b884e3acdead3c100
Hemanth R <hemanthrt18@gmail.com>
2023-05-13 23:30:47 +0530

    Update AmzSearch.jsx

--- src/components/AmzSearch/AmzSearch.jsx
+++ src/components/AmzSearch/AmzSearch.jsx
@@ -1,14 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { FiSearch } from "react-icons/fi";
+import axios from 'axios';
+
+import { UserContext } from '../../contexts/UserContext';
 
 function AmzSearch() {
 
     const [searchString, setSearchString] = useState("");
 
+    const { user, userToken } = useContext(UserContext)
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const searchUrl = `https://www.amazon.in/s?k=${searchString}`;
         // window.location.href = searchUrl;
+
+        if(user !== 'null' && searchString) {
+
+            const data = {
+                searchItem: searchString
+            }
+            axios.put(`${import.meta.env.VITE_APP_MAIN_API}/user/${user}/add-search-item`, data, {
+              headers: {
+                authorization: `Bearer ${userToken}`,
+              }
+            })
+              .then((res) => {
+                console.log(res)
+              }).catch(err => {
+                // alert(err.response.data.error)
+                console.log(err.response)
+              })
+          }
 
         chrome?.tabs?.create({ url: searchUrl });
     };
